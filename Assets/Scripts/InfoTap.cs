73da1a96eaf170eaeb3f447f0ed9df8b2e2bd4b0
Wisam Halawi <w_halawi@hotmail.co.uk>
2019-05-01 04:19:19 +0100

    view testmodel complete
    info text on tap functionality complete
    test model available to view
    fixed drag and follow speeds for movebygaze of menus in help and mainmenu
    multiple prefabs for ease

---
+++ Assets/Scripts/InfoTap.cs
@@ -0,0 +1,37 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoTap : MonoBehaviour, IInputClickHandler
+{
+
+    private bool showing = false;
+    public GameObject infoWindow;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (showing)
+        {
+            infoWindow.SetActive(false);
+            showing = false;
+        }
+        else
+        {
+            infoWindow.SetActive(true);
+            showing = true;
+        }
+    }
+}
